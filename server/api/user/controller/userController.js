7b615a83977504a46599dac41d40026a07b2c702
siddheshkadam1881 <siddheshkadam111@gmail.com>
2018-03-26 10:48:48 +0530

    working api

--- server/api/user/controller/userController.js
+++ server/api/user/controller/userController.js
@@ -11,7 +11,7 @@ var BUCKET_NAME = 'fundoo-notes-images';
 var fs = require('fs');
 var multer  = require('multer');
 import * as Parallel from 'async-parallel';
-// import parallel from './internal/parallel';
+var async=require('async');
 
 var storage =multer.diskStorage({
 destination :function(req,file,callback)
@@ -23,11 +23,26 @@ destination :function(req,file,callback)
    callback(null,file.originalname);
  },
  });
+
  var upload = multer({storage : storage}).single('profilePic');
 
 
-exports.Register = function(req, res) {
+exports.Register =  function(req, res) {
+async.parallel({
+  doc:function (callback) {
+    var user = new User({
+         username: req.body.username,
+         email: req.body.email,
+         userpass: req.body.userpass,
+         usermobile: req.body.usermobile,
+         dob: req.body.dob
+       });
 
+       user.save(function (err,doc) {
+         callback(err,doc)
+       });
+  },
+  s3Bucket:function (callback) {
     upload(req,res,function(err){
 
       var userObj = req.body || {};
@@ -37,69 +52,79 @@ exports.Register = function(req, res) {
       Bucket: BUCKET_NAME,
       Key: file.originalname,
       Body: file.path,
+      Expires: 60,
       ACL: 'public-read',
       ContentEncoding: 'base64', // required
       ContentType: `image`
       }
-       // console.log(params);
-      s3.upload(params, (err, data) => {
+       console.log(params);
+      s3.upload(params, callback);
+
+  })
+},function (err,result) {
+if (err)
+  return res.send({status:false});
+  console.log(result.s3Bucket.Location);
+}
+/* Not required
       if (err) { return console.log(err) }
       // Continue if no error
       // Save data.Location in your database
       console.log(data);
       console.log('Image successfully uploaded.');
       });
-
-
+      var url = s3.getSignedUrl('putObject', params);
+      console.log(url);
+*/
     //create user
-        try {
-        req.checkBody("username", "Enter Name.").notEmpty();
-        req.checkBody("email", "Enter a valid email address.").isEmail();
-        req.checkBody("userpass", "Enter a valid password").matches(/^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/);
-        req.checkBody("usermobile", "Enter a valid user mobile").matches(/^([7-9]{1}[0-9]{9})$/);
-        req.checkBody("dob",  "Enter date of birthday in DD/MM/YYYY format").matches(/^(\d{2})\/(\d{2})\/(\d{4})$/);
-        if (!req.file)
-        return res.status(400).send('No files were uploaded.');
-        var errors = req.validationErrors();
-        if (errors) {
-        throw errors;
-
-        }
-        else
-        {
-
-            var user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            userpass: req.body.userpass,
-            usermobile: req.body.usermobile,
-            dob: req.body.dob
-          });
-          // save the created user
-          user.save(function(err) {
-          try {
-            if (err) throw err;
-                          //console.log('User saved successfully');
-                          res.send({
-                              status: true,
-                              description: "User saved successfully"
-                          });
-                      } catch (e) {
-                        console.log(e);
-                          res.send({
-                              status: false,
-                              description: "Email Aready Exist"
-                          });
-                      }
-                  });
-        }
-    }
-     catch (e) {
-        console.log(e);
-        res.send({
-            status: false,
-            description: "Please fill the correct information"
-        });
-    }
+    //     try {
+    //     req.checkBody("username", "Enter Name.").notEmpty();
+    //     req.checkBody("email", "Enter a valid email address.").isEmail();
+    //     req.checkBody("userpass", "Enter a valid password").matches(/^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$/);
+    //     req.checkBody("usermobile", "Enter a valid user mobile").matches(/^([7-9]{1}[0-9]{9})$/);
+    //     req.checkBody("dob",  "Enter date of birthday in DD/MM/YYYY format").matches(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+    //     if (!req.file)
+    //     return res.status(400).send('No files were uploaded.');
+    //     var errors = req.validationErrors();
+    //     if (errors) {
+    //     throw errors;
+    //
+    //     }
+    //     else
+    //     {
+    //
+    //         var user = new User({
+    //         username: req.body.username,
+    //         email: req.body.email,
+    //         userpass: req.body.userpass,
+    //         usermobile: req.body.usermobile,
+    //         dob: req.body.dob
+    //       });
+    //       // save the created user
+    //       user.save(function(err) {
+    //       try {
+    //         if (err) throw err;
+    //                       //console.log('User saved successfully');
+    //                       res.send({
+    //                           status: true,
+    //                           description: "User saved successfully"
+    //                       });
+    //                   } catch (e) {
+    //                     console.log(e);
+    //                       res.send({
+    //                           status: false,
+    //                           description: "Email Aready Exist"
+    //                       });
+    //                   }
+    //               });
+    //     }
+    // }
+    //  catch (e) {
+    //     console.log(e);
+    //     res.send({
+    //         status: false,
+    //         description: "Please fill the correct information"
+    //     });
+    // }
     });
 };
